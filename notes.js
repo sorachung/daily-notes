816e2347a231e96497423be107d19816b47a2c51
sorachung <sorachung.src@gmail.com>
2021-11-16 11:42:56 -0600

    initial commit

---
+++ notes.js
@@ -0,0 +1,19 @@
+const notes = [
+    {
+        id: 1,
+        subject: "book1 ch.2",
+        date: 20211110,
+        feeling: "okay",
+        timeSpent: 300
+    },
+    {
+        id: 2,
+        subject: "book1 ch.4",
+        date: 20211111,
+        feeling: "good",
+        timeSpent: 330
+
+    }
+];
+
+console.log(notes);
