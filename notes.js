c7b38798fe4df7f1005829efbdc3f28fe4b07804
sorachung <sorachung.src@gmail.com>
2021-11-17 14:17:15 -0600

    added createNote function

--- notes.js
+++ notes.js
@@ -4,14 +4,14 @@ const notes = [
     subject: "HTML structure",
     date: 20211110,
     feeling: "great",
-    timeSpent: 300,
+    timeSpent: 300
   },
   {
     id: 2,
     subject: "git and GitHub",
     date: 20211111,
     feeling: "good",
-    timeSpent: 330,
+    timeSpent: 330
   },
 ];
 
@@ -20,7 +20,7 @@ const noteAboutToday = {
   subject: "Javascript data structures",
   date: 20211116,
   feeling: "great",
-  timeSpent: 60,
+  timeSpent: 60
 };
 
 notes.push(noteAboutToday);
@@ -43,3 +43,29 @@ for (const note of notes) {
     console.log(note);
   }
 }
+
+notes.forEach((note) => note.dateCreated = new Date());
+
+const newNote = {
+  subject: "Javascript functions",
+  date: 20211117,
+  feeling: "good",
+  timeSpent: 180
+}
+
+
+const createNote = (note) => {
+  const id = notes[notes.length - 1].id + 1;
+  note.id = id;
+
+  const today = new Date();
+  note.dateCreated = today;
+  notes.push(note);
+}
+
+
+createNote(newNote);
+
+console.log(notes);
+
+
